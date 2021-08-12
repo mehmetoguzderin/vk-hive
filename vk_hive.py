5f4cc77d05f542e3dd16ab466b644b927bfd730d
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-08-12 21:12:04 +0300

    Update vk_hive.py

--- vk_hive.py
+++ vk_hive.py
@@ -6,11 +6,13 @@ repositories = [
     "https://github.com/DTolm/VkFFT",
     "https://github.com/g-truc/glm",
     "https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator",
+    "https://github.com/jarro2783/cxxopts",
     "https://github.com/KhronosGroup/glslang",
     "https://github.com/KhronosGroup/KTX-Software",
     "https://github.com/KhronosGroup/SPIRV-Headers",
     "https://github.com/KhronosGroup/SPIRV-Tools",
     "https://github.com/KhronosGroup/Vulkan-Headers",
+    "https://github.com/nlohmann/json",
     "https://github.com/syoyo/tinygltf",
     "https://github.com/tinyobjloader/tinyobjloader",
 ]
